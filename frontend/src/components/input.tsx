2f2e8f84b79a9bb2ef4391828483c85fae2c5d81
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-03-20 20:23:39 -0300

    feat: input type password

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,21 +1,32 @@
-import { AlertCircleIcon } from "@hugeicons/core-free-icons";
+import { AlertCircleIcon, ViewIcon, ViewOffIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputRootProps extends ComponentProps<"div"> {
   error?: string;
+  label?: string;
 }
 
-export function InputRoot({ error, children, ...props }: InputRootProps) {
+export function InputRoot({
+  label,
+  error,
+  children,
+  ...props
+}: InputRootProps) {
   return (
-    <div data-error={!!error} className="group" {...props}>
+    <div
+      data-error={!!error}
+      className="group flex-1"
+      {...props}
+    >
       <label
         className="text-label-md uppercase text-gray-200 group-focus-within:text-orange-base"
         htmlFor=""
       >
-        label
+        {label}
       </label>
-      <div className="relative">{children}</div>
+      <div className="relative flex">{children}</div>
       {!!error && (
         <span className="flex gap-1 mt-1 items-center text-body-xs text-danger">
           <HugeiconsIcon icon={AlertCircleIcon} />
@@ -26,21 +37,37 @@ export function InputRoot({ error, children, ...props }: InputRootProps) {
   );
 }
 
-export function InputIcon(props: ComponentProps<"span">) {
+export function InputIcon({ className, ...props }: ComponentProps<"span">) {
   return (
     <span
-      className="size-6 text-gray-200 group-focus-within:text-orange-base absolute top-1/2 transform -translate-y-1/2 group-[&:not(:has(input:placeholder-shown))]:text-orange-base group-data-[error='true']:text-danger"
+      className={twMerge(
+        "size-6 text-gray-200 group-focus-within:text-orange-base absolute top-1/2 transform -translate-y-1/2 group-[&:not(:has(input:placeholder-shown))]:text-orange-base group-data-[error='true']:text-danger",
+        className
+      )}
       {...props}
     />
   );
 }
 
-export function InputField(props: ComponentProps<"input">) {
+interface InputFieldProps extends ComponentProps<"input"> {
+  password?: boolean
+}
+
+
+export function InputField({type, password, ...props }: InputFieldProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(!password)
+
   return (
-    <input
-      placeholder="Placeholder"
-      className="p-2 pl-8 border-b-2 caret-orange-base border-gray-100 outline-0 focus-within:border-gray-400"
-      {...props}
-    />
+    <>
+      <InputIcon onClick={() => setIsPasswordVisible(prevState => !prevState)} data-password={password} className="absolute hidden right-0 data-[password='true']:flex">
+        <HugeiconsIcon icon={isPasswordVisible ? ViewOffIcon: ViewIcon} />
+      </InputIcon>
+      <input
+        placeholder="Placeholder"
+        type={isPasswordVisible ? "password" : type}
+        className="flex-1 p-2 pl-8 border-b-2 caret-orange-base border-gray-100 outline-0 focus-within:border-gray-400"
+        {...props}
+      />
+    </>
   );
 }
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,24 +1,38 @@
 import { HugeiconsIcon } from "@hugeicons/react";
 import { InputField, InputIcon, InputRoot } from "../../components/input";
-import { Mail02Icon } from "@hugeicons/core-free-icons";
+import { AccessIcon, Mail02Icon } from "@hugeicons/core-free-icons";
 
 export function SignIn() {
   return (
     <form action="">
-      <div>
-        <h1 className="font-sans text-2xl text-gray-500 font-medium">Acesse sua conta</h1>
-        <span className="mt-2 font-poppins text-gray-300">Informe seu e-mail e senha para entrar</span>
-
+      <div className="flex flex-col gap-12">
         <div>
-          <InputRoot>
+          <h1 className="font-sans text-2xl text-gray-500 font-medium">
+            Acesse sua conta
+          </h1>
+          <span className="mt-2 font-poppins text-gray-300">
+            Informe seu e-mail e senha para entrar
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <InputRoot label="E-mail">
             <InputIcon>
               <HugeiconsIcon icon={Mail02Icon} />
             </InputIcon>
 
-            <InputField placeholder="" />
+            <InputField placeholder="Seu e-mail cadastrado" />
+          </InputRoot>
+
+          <InputRoot label="Senha">
+            <InputIcon>
+              <HugeiconsIcon icon={AccessIcon} />
+            </InputIcon>
+
+            <InputField password placeholder="Sua senha de acesso" />
           </InputRoot>
         </div>
       </div>
     </form>
-  )
+  );
 }
